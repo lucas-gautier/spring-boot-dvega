5ab0e3ffd0bd43bc29feb2224be849ed5658dde6
lucas-gautier <4118576+lucas-gautier@users.noreply.github.com>
2021-10-05 21:21:15 -0500

    feature: added slf4j logging

--- src/main/java/lucas/springbootdvega/SpringBootDvegaApplication.java
+++ src/main/java/lucas/springbootdvega/SpringBootDvegaApplication.java
@@ -1,6 +1,8 @@
 package lucas.springbootdvega;
 
 import lucas.springbootdvega.config.Properties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableConfigurationProperties(Properties.class)
@@ -15,6 +20,11 @@ public class SpringBootDvegaApplication {
 
     @Autowired
     private Properties properties;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    public static final Logger log = LoggerFactory.getLogger(SpringBootDvegaApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDvegaApplication.class, args);
@@ -27,6 +37,16 @@ public class SpringBootDvegaApplication {
         return args -> {
             // Do something after start up, print welcome message from Properties class
             System.out.println("Welcome message: " + properties.getWelcomeMessage());
+
+            // Print out all bean names in application context
+            String[] beans = applicationContext.getBeanDefinitionNames();
+            Arrays.sort(beans);
+            for(String bean: beans) {
+                System.out.println(bean);
+            }
+
+            // Print logs via slf4j
+            log.info("CommandLineRunner.run();");
         };
     }
 }
